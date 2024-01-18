f3a847628f0e753f3e728675668bd30d7b0cdb3d
MrColorado35 <stan.kaczorkiewicz@yahoo.co.uk>
2024-01-18 20:28:28 +0000

    Solve the time couting issue

--- main.py
+++ main.py
@@ -46,16 +46,45 @@ class Outplayed:
         driver = webdriver.Chrome(service=service, options=options)
         return driver
 
+    # Once you are on the right page, press two buttons to get to teh required data
     def get_tennis(self):
+        # Find required button
         btn_tennis = self.driver.find_element(By.XPATH, "//div[contains(@class, 'main-items')]//vn-menu-item//a[contains(text(), 'Tennis')]")
+        # Simulate clicking
         btn_tennis.click()
+        # Allow the page to load
         sleep(4)
+        # Repeat last 3 steps with another button
         btn_all = self.driver.find_element(By.XPATH, "//div[contains(@class, 'ms-top-items-widget')]//div[contains(@class, 'list-all')]//a[contains(@class, 'ms-active-highlight')][1]")
         btn_all.click()
         sleep(5)
+        # inform the Programmer about the success
         print("Got to the 'all competitions' page ")
 
+    # Collect details about competitions
     def get_competitions(self):
+
+        pass
+
+    # collect details about the time
+    def get_time(self, time_text):
+        # Get the current UTC time
+        current_utc_time = datetime.utcnow()
+
+        # Parse the input time
+        event_time_str = time_text
+        event_time = datetime.strptime(event_time_str, "%Y-%m-%d %I:%M %p")
+
+        # Calculate the time difference between now and the event time
+        time_difference = event_time - current_utc_time
+
+        # If the event is today, add 1 day to the event time
+        if time_difference.days == 0 and time_difference.seconds < 0:
+            event_time += timedelta(days=1)
+        # Print the event time in the desired format (yyyy-mm-dd hh:mm)
+        formatted_event_time = event_time.strftime("%Y-%m-%d %H:%M")
+        return formatted_event_time, current_utc_time
+
 
 
 
